09202322bfcaebc6f4a82f7d13ae98c516a14e7b
sqjin <32399212+sqjin@users.noreply.github.com>
2019-10-23 16:07:09 -0700

    Update scAI_plotting.R

--- R/scAI_plotting.R
+++ R/scAI_plotting.R
@@ -10,8 +10,6 @@ scAI_theme_opts <- function() {
   theme(strip.background = element_rect(colour = "white", fill = "white")) +
     theme_classic() +
     theme(panel.border = element_blank()) +
-    theme(axis.line.x = element_line(color = "black")) +
-    theme(axis.line.y = element_line(color = "black")) +
     theme(panel.grid.minor.x = element_blank(), panel.grid.minor.y = element_blank()) +
     theme(panel.grid.major.x = element_blank(), panel.grid.major.y = element_blank()) +
     theme(panel.background = element_rect(fill = "white")) +
